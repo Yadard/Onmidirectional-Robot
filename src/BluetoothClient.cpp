b8ea251e53f15ac70ab8e2da140d734b73c53633
Yadard <eduardomachadobehling@gmail.com>
2022-09-11 13:51:02 -0300

    added bluetooth client

---
+++ src/BluetoothClient.cpp
@@ -0,0 +1,27 @@
+#include <BluetoothClient.h>
+
+String BluetoothClient::BluetoothClient::readString() {
+    String str((char *)(this->buffer.data));
+    buffer.erase_back(str.length());
+    --this->arrived;
+    return str;
+}
+
+void BluetoothClient::BluetoothClient::operator()() {
+    if (this->available()) {
+        const int16_t data = this->read();
+        if (data != -1) {
+            if (data == '{') {
+                this->reading = true;
+            } else if (data == '}') {
+                buffer.push_back('\0');
+                ++this->arrived;
+                this->reading = false;
+            } else if (this->reading) {
+                buffer.push_back(static_cast<byte>(data));
+            }
+        }
+    }
+}
+
+bool BluetoothClient::BluetoothClient::arrivedMessage() { return this->arrived; }
