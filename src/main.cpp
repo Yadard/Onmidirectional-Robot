18c94936bc8a8aa7438e7c7c93fa339f9eac2f30
Yadard <eduardomachadobehling@gmail.com>
2022-09-13 15:39:42 -0300

    Added ultrasonic.h

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,57 @@
 #include <Logger.h>
 #include <Motor.h>
 #include <Movement.h>
-#include <Ultrasonic.h>
 
 Logger logger;
 
 #define LOG_EXPR(l, x) logger.log(l, '[', #x, "]: ", x)
 
-#define trigPin 2
-#define echoPin A3
+Event::Base *parseEvent(String &message, Movement::Base *mov, Motor::Base *motors) {
+    String values = message.substring(2);
+    size_t separator;
+    Motor::ContinuousRotationServo *array;
+    switch (message[0]) {
+    case 'J':
+        LOG_EXPR(Logger::LogLevel::DEBUG, 'J');
+        separator = values.indexOf(',');
+        LOG_EXPR(Logger::LogLevel::DEBUG, values.substring(0, separator).toInt());
+        LOG_EXPR(Logger::LogLevel::DEBUG, values.substring(separator + 1).toInt());
+        return new Event::Movement(mov, values.substring(0, separator).toInt(), values.substring(separator + 1).toInt());
+        break;
 
-Ultrasonic::Sensor sensor(trigPin, A3, A4, A5);
+    case 'A':
+    case 'B':
+    case 'C':
+        LOG_EXPR(Logger::LogLevel::DEBUG, values);
+        array = static_cast<Motor::ContinuousRotationServo *>(motors);
+        return new Event::Motor(&array[message[0] - 'A'], values);
+        break;
+
+    default:
+        break;
+    }
+    return nullptr;
+}
+
+BluetoothClient::BluetoothClient bt_client(10, 11);
+
 void setup() {
     Serial.begin(9600);
+    bt_client.begin(9600);
     logger.setLogLevel(Logger::LogLevel::DEBUG);
 }
 
 void loop() {
-    static uint32_t duration, distance;
+    static Motor::ContinuousRotationServo motors[] = {{3, 160}, {5, 170}, {6, 185}};
+    static Movement::Onmidirectional_3Wheels movement(&motors[0], &motors[1], &motors[2]);
+    static Event::Base *event;
 
-    auto result = sensor.read();
-
-    LOG_EXPR(Logger::LogLevel::DEBUG, result.cm_front);
-    LOG_EXPR(Logger::LogLevel::DEBUG, result.cm_left);
-    LOG_EXPR(Logger::LogLevel::DEBUG, result.cm_right);
-    logger.log(Logger::LogLevel::DEBUG, "=============================");
-
-    delay(50);
+    bt_client();
+    if (bt_client.arrivedMessage()) {
+        auto message = bt_client.readMessage();
+        LOG_EXPR(Logger::LogLevel::DEBUG, message);
+        event = parseEvent(message, &movement, (Motor::Base *)motors);
+        if (event)
+            event->operator()();
+    }
 }
