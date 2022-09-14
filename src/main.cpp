fd6a2b116aa79ab6af771b3a037b0918d44f2b6d
Yadard <eduardomachadobehling@gmail.com>
2022-09-14 06:19:31 -0300

    Now it's good

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ class Robo {
     void setSpeed(float x, float y) {
         speed_base.x = x;
         speed_base.y = y;
+        speed.x = x;
+        speed.y = y;
     }
 
     void addSpeed(float x, float y) {
@@ -71,6 +73,9 @@ void loop() {
     static AntiCollisionSystem::Base *anti_collision_system = &tri_ultrasonic;
     static Robo robo(&movement);
 
+    static const uint16_t angles[] = {30, 150, 270};
+    static const uint16_t margin_sensor = 10;
+
     bt_client();
 
     if (bt_client.arrivedMessage()) {
@@ -82,18 +87,61 @@ void loop() {
     auto data = anti_collision_system->read(5);
     logger.log(Logger::LogLevel::DEBUG, "====================");
 
-    if (!(data.cm_front && data.cm_front < 20) && !(data.cm_left && data.cm_left < 20) && !(data.cm_right && data.cm_right < 20)) {
-        robo.addSpeed(0, 0);
-    } else if (data.cm_front && data.cm_front < 20 && (robo.getSpeed().x || robo.getSpeed().y)) {
-        logger.log(Logger::LogLevel::DEBUG, "front");
-        robo.addSpeed(map(data.cm_front, 20, 0, 50, 100) * cos(radians(60)), map(data.cm_front, 20, 0, 50, 100) * sin(radians(60)));
-    } else if (data.cm_left && data.cm_left < 20 && (robo.getSpeed().x || robo.getSpeed().y)) {
-        logger.log(Logger::LogLevel::DEBUG, "left");
-        robo.addSpeed(map(data.cm_left, 20, 0, 50, 100) * cos(radians(300)), map(data.cm_left, 20, 0, 50, 100) * sin(radians(300)));
-    } else if (data.cm_right && data.cm_right < 20 && (robo.getSpeed().x || robo.getSpeed().y)) {
-        logger.log(Logger::LogLevel::DEBUG, "right");
-        robo.addSpeed(map(data.cm_right, 20, 0, 50, 100) * cos(radians(180)), map(data.cm_right, 20, 0, 50, 100) * sin(radians(180)));
+    if (!data.cm_front)
+        data.cm_front = 5000;
+    if (!data.cm_left)
+        data.cm_left = 5000;
+    if (!data.cm_right)
+        data.cm_right = 5000;
+
+    if (robo.getSpeed().x || robo.getSpeed().y) {
+        if (data.cm_front < data.cm_left) {
+            if (data.cm_front < data.cm_right) {
+                if (data.cm_front < margin_sensor) {
+                    logger.log(Logger::LogLevel::DEBUG, "front");
+                    robo.addSpeed(map(data.cm_front, margin_sensor, 0, 50, 200) * cos(radians(angles[0])),
+                                  map(data.cm_front, margin_sensor, 0, 50, 200) * sin(radians(angles[0])));
+                }
+            } else {
+                if (data.cm_right < margin_sensor) {
+                    logger.log(Logger::LogLevel::DEBUG, "right");
+                    robo.addSpeed(map(data.cm_right, margin_sensor, 0, 50, 200) * cos(radians(angles[2])),
+                                  map(data.cm_right, margin_sensor, 0, 50, 200) * sin(radians(angles[2])));
+                }
+            }
+        } else {
+            if (data.cm_left < data.cm_right) {
+                if (data.cm_left < margin_sensor) {
+                    logger.log(Logger::LogLevel::DEBUG, "left");
+                    robo.addSpeed(map(data.cm_left, margin_sensor, 0, 50, 200) * cos(radians(angles[1])),
+                                  map(data.cm_left, margin_sensor, 0, 50, 200) * sin(radians(angles[1])));
+                }
+            } else {
+                if (data.cm_right < margin_sensor) {
+                    logger.log(Logger::LogLevel::DEBUG, "right");
+                    robo.addSpeed(map(data.cm_right, margin_sensor, 0, 50, 200) * cos(radians(angles[2])),
+                                  map(data.cm_right, margin_sensor, 0, 50, 200) * sin(radians(angles[2])));
+                }
+            }
+        }
     }
+
+    // if (!(data.cm_front && data.cm_front < margin_sensor) && !(data.cm_left && data.cm_left < margin_sensor) &&
+    //     !(data.cm_right && data.cm_right < margin_sensor)) {
+    //     robo.addSpeed(0, 0);
+    // } else if (data.cm_front && data.cm_front < margin_sensor && (robo.getSpeed().x || robo.getSpeed().y)) {
+    //     logger.log(Logger::LogLevel::DEBUG, "front");
+    //     robo.addSpeed(map(data.cm_front, margin_sensor, 0, 50, 200) * cos(radians(angles[0])),
+    //                   map(data.cm_front, margin_sensor, 0, 50, 200) * sin(radians(angles[0])));
+    // } else if (data.cm_left && data.cm_left < margin_sensor && (robo.getSpeed().x || robo.getSpeed().y)) {
+    //     logger.log(Logger::LogLevel::DEBUG, "left");
+    //     robo.addSpeed(map(data.cm_left, margin_sensor, 0, 50, 200) * cos(radians(angles[1])),
+    //                   map(data.cm_left, margin_sensor, 0, 50, 200) * sin(radians(angles[1])));
+    // } else if (data.cm_right && data.cm_right < margin_sensor && (robo.getSpeed().x || robo.getSpeed().y)) {
+    //     logger.log(Logger::LogLevel::DEBUG, "right");
+    //     robo.addSpeed(map(data.cm_right, margin_sensor, 0, 50, 200) * cos(radians(angles[2])),
+    //                   map(data.cm_right, margin_sensor, 0, 50, 200) * sin(radians(angles[2])));
+    // }
 
     LOG_EXPR(Logger::LogLevel::DEBUG, data.cm_front);
     LOG_EXPR(Logger::LogLevel::DEBUG, data.cm_left);
