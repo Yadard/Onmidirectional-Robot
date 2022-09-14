a8a0f92d6198b8f41d0d37dfc5169818980955fe
Yadard <eduardomachadobehling@gmail.com>
2022-09-14 02:46:37 -0300

    Final touch

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,42 @@ Logger logger;
 
 #define LOG_EXPR(l, x) logger.log(l, '[', #x, "]: ", x)
 
-float bt_fx = 0, bt_fy = 0;
+class Robo {
+  public:
+    Robo(Movement::Base *mov) : mov(mov) {}
 
-Event::Base *parseEvent(String &message, Movement::Base *mov, Motor::Base *motors) {
+    void setSpeed(float x, float y) {
+        speed_base.x = x;
+        speed_base.y = y;
+    }
+
+    void addSpeed(float x, float y) {
+        speed.x = speed_base.x + x;
+        speed.y = speed_base.y + y;
+    }
+
+    struct Vector;
+    const Vector &getSpeed() { return speed; }
+
+    void operator()() { mov->applySpeed(speed.x, speed.y); }
+
+  private:
+    Movement::Base *mov;
+    struct Vector {
+        float x = 0, y = 0;
+    } speed_base, speed;
+};
+
+Event::Base *parseEvent(String &message, Robo *robo) {
     String values = message.substring(2);
     size_t separator;
-    Motor::ContinuousRotationServo *array;
     switch (message[0]) {
     case 'J':
         LOG_EXPR(Logger::LogLevel::DEBUG, 'J');
         separator = values.indexOf(',');
         LOG_EXPR(Logger::LogLevel::DEBUG, values.substring(0, separator).toInt());
         LOG_EXPR(Logger::LogLevel::DEBUG, values.substring(separator + 1).toInt());
-        bt_fx = values.substring(0, separator).toInt();
-        bt_fy = values.substring(separator + 1).toInt();
-        break;
-
-    case 'A':
-    case 'B':
-    case 'C':
-        LOG_EXPR(Logger::LogLevel::DEBUG, values);
-        array = static_cast<Motor::ContinuousRotationServo *>(motors);
-        return new Event::Motor(&array[message[0] - 'A'], values);
+        robo->setSpeed(values.substring(0, separator).toInt(), values.substring(separator + 1).toInt());
         break;
 
     default:
@@ -49,52 +63,41 @@ void setup() {
 }
 
 void loop() {
-    static Motor::ContinuousRotationServo motors[] = {{3, 160}, {5, 170}, {6, 185}};
+    static Motor::ContinuousRotationServo motors[] = {{3, 170}, {5, 175}, {6, 190}};
     static Movement::Onmidirectional_3Wheels movement(&motors[0], &motors[1], &motors[2]);
-    static Event::Base *event;
     static Ultrasonic::Sensor ultrasonic(2, A3, A4, A5);
-    static uint32_t timer_start = 0;
+    static Robo robo(&movement);
 
     bt_client();
-    uint32_t timer_end = millis();
+
     if (bt_client.arrivedMessage()) {
-        timer_start = timer_end;
         auto message = bt_client.readMessage();
         LOG_EXPR(Logger::LogLevel::DEBUG, message);
-        event = parseEvent(message, &movement, (Motor::Base *)motors);
-        if (event)
-            event->operator()();
+        parseEvent(message, &robo);
     }
 
     auto data = ultrasonic.read();
-    float fx = bt_fx, fy = bt_fy;
-
     logger.log(Logger::LogLevel::DEBUG, "====================");
-    if (data.cm_front && data.cm_front < 20) {
+    if (!(data.cm_front && data.cm_front < 20) && !(data.cm_left && data.cm_left < 20) && !(data.cm_right && data.cm_right < 20)) {
+        robo.addSpeed(0, 0);
+    } else if (data.cm_front && data.cm_front < 20) {
         logger.log(Logger::LogLevel::DEBUG, "front");
-        fx += map(data.cm_front, 20, 0, 50, 100) * cos(radians(60));
-        fy += map(data.cm_front, 20, 0, 50, 100) * sin(radians(60));
-    }
-    if (data.cm_left && data.cm_left < 20) {
+        robo.addSpeed(map(data.cm_front, 20, 0, 50, 100) * cos(radians(60)), map(data.cm_front, 20, 0, 50, 100) * sin(radians(60)));
+    } else if (data.cm_left && data.cm_left < 20) {
         logger.log(Logger::LogLevel::DEBUG, "left");
-        fx += map(data.cm_left, 20, 0, 50, 100) * cos(radians(300));
-        fy += map(data.cm_left, 20, 0, 50, 100) * sin(radians(300));
-    }
-    if (data.cm_right && data.cm_right < 20) {
+        robo.addSpeed(map(data.cm_left, 20, 0, 50, 100) * cos(radians(300)), map(data.cm_left, 20, 0, 50, 100) * sin(radians(300)));
+    } else if (data.cm_right && data.cm_right < 20) {
         logger.log(Logger::LogLevel::DEBUG, "right");
-        fx += map(data.cm_right, 20, 0, 50, 100) * cos(radians(180));
-        fy += map(data.cm_right, 20, 0, 50, 100) * sin(radians(180));
+        robo.addSpeed(map(data.cm_right, 20, 0, 50, 100) * cos(radians(180)), map(data.cm_right, 20, 0, 50, 100) * sin(radians(180)));
     }
 
     LOG_EXPR(Logger::LogLevel::DEBUG, data.cm_front);
     LOG_EXPR(Logger::LogLevel::DEBUG, data.cm_left);
     LOG_EXPR(Logger::LogLevel::DEBUG, data.cm_right);
     logger.log(Logger::LogLevel::DEBUG, "--------------------");
-    LOG_EXPR(Logger::LogLevel::DEBUG, bt_fx);
-    LOG_EXPR(Logger::LogLevel::DEBUG, bt_fy);
-    LOG_EXPR(Logger::LogLevel::DEBUG, fx);
-    LOG_EXPR(Logger::LogLevel::DEBUG, fy);
+    LOG_EXPR(Logger::LogLevel::DEBUG, robo.getSpeed().x);
+    LOG_EXPR(Logger::LogLevel::DEBUG, robo.getSpeed().y);
     logger.log(Logger::LogLevel::DEBUG, "====================");
 
-    movement.applySpeed(fx, fy);
+    robo();
 }
